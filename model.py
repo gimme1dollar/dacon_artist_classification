996035a719bd93dd26f93412a2bdb18ab05135bf
JYLee <gimme1dollar@postech.ac.kr>
2022-10-26 21:25:45 +0900

    add argparse

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import torchvision.models as models
 
 class BaseModel(nn.Module):
-    def __init__(self, num_classes=len(le.classes_)):
+    def __init__(self, num_classes=50):
         super(BaseModel, self).__init__()
         self.backbone = models.efficientnet_b0(pretrained=True)
         self.classifier = nn.Linear(1000, num_classes)
--- train.py
+++ train.py
@@ -1,33 +1,27 @@
-import random
-import pandas as pd
-import numpy as np
 import os
 import cv2
-
+import random
+import argparse
+import pandas as pd
+import numpy as np
+from tqdm.auto import tqdm
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
 import torch
 import torch.nn as nn
+import albumentations as A 
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-
-from tqdm.auto import tqdm
-
-import albumentations as A 
 from albumentations.pytorch.transforms import ToTensorV2
 
 from .utils import *
+from .model import *
+from .dataset import *
 
 import warnings
 warnings.filterwarnings(action='ignore') 
-
-
-def get_data(df, infer=False):
-    if infer:
-        return df['img_path'].values
-    return df['img_path'].values, df['artist'].values
 
 
 
@@ -69,13 +63,12 @@ def train(model, optimizer, train_loader, test_loader, scheduler, device):
             best_model = model
             best_score = val_score
 
-#             torch.save({'epoch':epoch,
-#                         'model_state_dict':model.state_dict(), 
-#                         'optimizer_state_dict':optimizer.state_dict(),
-#                         'loss':val_loss,
-#                         },
-#                         f"/content/drive/MyDrive/eff/1023_{epoch}_{val_score:.3f}.pt")
-            
+            torch.save({'epoch':epoch,
+                        'model_state_dict':model.state_dict(), 
+                        'optimizer_state_dict':optimizer.state_dict(),
+                        'loss':val_loss,
+                        },
+                        f"/content/drive/MyDrive/eff/1023_{epoch}_{val_score:.3f}.pt")
         
     return best_model
 
@@ -105,7 +98,6 @@ def validation(model, criterion, test_loader, device):
     return np.mean(val_loss), val_f1
 
 
-
 def inference(model, test_loader, device):
     model.to(device)
     model.eval()
@@ -122,80 +114,45 @@ def inference(model, test_loader, device):
     print('Done.')
     return model_preds
 
-
-
 if __name__ == "__main__":
-    
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    CFG = {
-    'IMG_SIZE':224,
-    'EPOCHS':20,
-    'LEARNING_RATE':3e-4,
-    'BATCH_SIZE':64,
-    'SEED':41
-    }
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--data_dir', type=str, default='data/')
+    parser.add_argument('--device', type=str, default='cuda:0')
+    parser.add_argument('--epoch', type=int, default=20)
+    parser.add_argument('--image_size', type=int, default=224)
+    parser.add_argument('--learning_rate', type=float, default=3e-4)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--seed', type=int, default=41)
+    args = parser.parse_args()
+    if not torch.cuda.is_available():
+        args.device = torch.device('cpu')
+    seed_everything(args.seed)
 
-    seed_everything(CFG['SEED']) # Seed 고정
+    # Dataset
+    train_dataset, val_dataset,
+        train_transform, test_transform = get_dataset()
+    train_loader = DataLoader(train_dataset, batch_size =args.batch_size,shuffle=True, num_workers=0)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
 
-
-    df = pd.read_csv('train.csv')
-    le = preprocessing.LabelEncoder()
-    df['artist'] = le.fit_transform(df['artist'].values)
-
-    train_df, val_df, _, _ = train_test_split(df, df['artist'].values, test_size=0.2, random_state=CFG['SEED'])
-
-    train_df = train_df.sort_values(by=['id'])
-
-    val_df = val_df.sort_values(by=['id'])
-
-    train_img_paths, train_labels = get_data(train_df)
-    val_img_paths, val_labels = get_data(val_df)
-
-    train_transform = A.Compose([
-    A.Resize(480,480), # 299
-    A.RandomCrop(224,224),
-    A.HorizontalFlip(p = 0.5), 
-    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
-    ToTensorV2()
-    ])
-
-    test_transform = A.Compose([
-    A.Resize(480, 480),
-    A.RandomCrop(224, 224),
-    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
-    ToTensorV2()
-    ])
-
-
-    train_dataset = CustomDataset(train_img_paths, train_labels, train_transform)
-    train_loader = DataLoader(train_dataset, batch_size = CFG['BATCH_SIZE'], shuffle=True, num_workers=0)
-
-    val_dataset = CustomDataset(val_img_paths, val_labels, test_transform)
-    val_loader = DataLoader(val_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=False, num_workers=0)
-
-    model = BaseModel()
+    # Train model
+    model = BaseModel(num_classes=len(le.classes))
     model.eval()
     optimizer = torch.optim.Adam(params = model.parameters(), lr = CFG["LEARNING_RATE"])
     scheduler = None 
 
-    infer_model = train(model, optimizer, train_loader, val_loader, scheduler, device)
+    trained_model = train(model, optimizer, train_loader, val_loader, scheduler, args.device)
 
+    # Inference models
     test_df = pd.read_csv('test.csv')
-
     test_img_paths = get_data(test_df, infer=True)
 
     test_dataset = CustomDataset(test_img_paths, None, test_transform)
-    test_loader = DataLoader(test_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=False, num_workers=0)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
 
-
-    preds = inference(infer_model, test_loader, device)
-
-
-
+    # Submit
+    preds = inference(trained_model, test_loader, args.device)
     preds = le.inverse_transform(preds) # LabelEncoder로 변환 된 Label을 다시 화가이름으로 변환
 
     submit = pd.read_csv('sample_submission.csv')
-
     submit['artist'] = preds
-
     submit.to_csv('sample/sample_1026.csv', index = False)
